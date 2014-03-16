bdd82fbd1341eac157d4151a0b7248c1c6feecf8
Cindi Thompson <cindi.thompson@gmail.com>
2014-03-16 13:11:51 -0700

    Added P/R and XValidation
    Added Hao's machinery plus more to (1) do cross-validation, (2) train
    on one XML file and test on another, (3) compute P/R, and F1. This
    won't match exactly the score they compute, but it's good enough to
    help pick features and classifiers.

--- semevalTask4.py
+++ semevalTask4.py
@@ -6,6 +6,9 @@ import scipy
 from nltk.corpus import conll2000
 from nltk.chunk.util import conlltags2tree
 import re
+import cPickle
+from sklearn import cross_validation
+
 
 #We are not using this, it's just an example to play around with chunking from the NLTK book
 class UnigramChunker(nltk.ChunkParserI):
@@ -76,20 +79,25 @@ class ConsecutiveChunker(nltk.ChunkParserI):
         """
         tagged_sents = self.tagger.tag(sentence)
         #print "parse result:", tagged_sents
-        conlltags = [(w, t, c) for ((w, t), c) in tagged_sents]
+        return [(w, t, c) for ((w, t), c) in tagged_sents]
+        #conlltags = [(w, t, c) for ((w, t), c) in tagged_sents]
         #print "conlltags:", conlltags
-        return conlltags2tree(conlltags)
+        #return conlltags2tree(conlltags)
 
     def evaluate(self, gold):
-        """ nltk machinery computes Acc, P,R, and F-measure for trees.
+        """ nltk machinery computes Acc, P,R, and F-measure for trees. But we are not using it!
         """
-        chunkscore = nltk.ChunkScore()
+        #chunkscore = nltk.ChunkScore()
+        results = []
         for tagged_sent in gold:
-            print "true:", tagged_sent
-            print "guess:", self.parse([(w,t) for (w,t,_c) in tagged_sent])
+            #print "true:", tagged_sent
+            guess = self.parse([(w,t) for (w,t,_c) in tagged_sent])
+            #print "guess:", guess
+            results.append(guess)
             #score thinks things should be in trees
-            chunkscore.score(conlltags2tree(tagged_sent), self.parse([(w,t) for (w,t,_c) in tagged_sent]))
-        return chunkscore
+            #chunkscore.score(conlltags2tree(tagged_sent), self.parse([(w,t) for (w,t,_c) in tagged_sent]))
+        #return chunkscore
+        return results
 
 
 def chunk_features(sentence, i, sent_dict, history):
@@ -143,7 +151,99 @@ def train_and_test(filename, posit_lex_file='positive-words.txt', nega_lex_file=
     posi_words = get_liu_lexicon(posit_lex_file)
     negi_words = get_liu_lexicon(nega_lex_file)
     chunker = ConsecutiveChunker(train, {'pos': posi_words, 'neg': negi_words})
-    print chunker.evaluate(test)
+    guessed_iobs = chunker.evaluate(test)
+    compute_pr(test, guessed_iobs)
+    #print chunker.evaluate(test)
+
+
+def train_and_trial(trn_file, test_file, posit_lex_file='positive-words.txt', nega_lex_file='negative-words.txt', pickled=False):
+    """ Train on the training file and test on the testing file
+    """
+    if pickled:
+        f = open(trn_file, 'rb')
+        traind = cPickle.load(f)
+        f.close()
+        f = open(test_file, 'rb')
+        testd = cPickle.load(f)
+        f.close()
+    else:
+        traind = XMLParser.create_exs(trn_file)
+        testd = XMLParser.create_exs(test_file)
+    posi_words = get_liu_lexicon(posit_lex_file)
+    negi_words = get_liu_lexicon(nega_lex_file)
+    chunker = ConsecutiveChunker(traind['iob'], {'pos': posi_words, 'neg': negi_words})
+    print "done training"
+    f = open('learned.pkl','wb')
+    cPickle.dump(chunker,f)
+    f.close()
+    guessed_iobs = chunker.evaluate(testd['iob'])
+    XMLParser.create_xml(testd['orig'],guessed_iobs,testd['id'],testd['idx'],'trial_answers.xml')
+    compute_pr(testd['iob'], guessed_iobs)
+
+
+def K_fold_train_and_test(filename, posit_lex_file='positive-words.txt', nega_lex_file='negative-words.txt', k=2, pickled=False):
+    """Does K-fold cross-validation on the given filename
+    """
+    if pickled:
+        f = open(filename, 'rb')
+        traind = cPickle.load(f)
+        f.close()
+    else:
+        traind = XMLParser.create_exs(filename)
+    n = len(traind['iob'])
+    posi_words = get_liu_lexicon(posit_lex_file)
+    negi_words = get_liu_lexicon(nega_lex_file)
+    kf = cross_validation.KFold(n, n_folds=k, indices=True)
+    for train, test in kf:
+        print "next fold, split size: %d/%d" %(len(train), len(test))
+        #print train
+        train_set = []
+        test_set = []
+        for i in train:
+            train_set.append(traind['iob'][i])
+        for i in test:
+            test_set.append(traind['iob'][i])
+        chunker = ConsecutiveChunker(train_set, {'pos': posi_words, 'neg': negi_words})
+        guesses = chunker.evaluate(test_set)
+        #print test_set
+        #print guesses
+        compute_pr(test_set, guesses)
+
+
+def compute_pr(c_iobs, g_iobs):
+    """Compute precision, recall, and F1
+    """
+    tp = 0
+    fp = 0
+    n = 0
+    print "num examples:", len(c_iobs)
+    #c_iobs = corrects['iob']
+    #g_iobs = guesses['iob']
+    for i in range(len(c_iobs)):
+        one_corr = c_iobs[i]
+        one_guess = g_iobs[i]
+        #just counting the first word in each aspect phrase
+        begin_terms = [x[0] for x in one_corr if x[2].startswith('B')]
+        n += len(begin_terms)
+        #print "n after %s: %d" %(one_corr, n)
+        #print "corr", begin_terms
+        for j in range(len(one_guess)):
+            if one_guess[j][2].startswith('B'):
+                #print "guess", one_guess[j]
+                if one_guess[j][0] in begin_terms:
+                    tp += 1
+                else:
+                    fp += 1
+    recall = float(tp)/n
+    if (tp+fp)>0:
+        precision = float(tp)/(tp + fp)
+    else:
+        precision = 1.0
+    if tp > 0:
+        f1 = (2 * precision * recall)/(precision + recall)
+    else:
+        f1 = 0
+    print "Recall %f0.2, Precision: %f0.2, F1 %f0.2" %(recall, precision, f1)
 
 
 def get_liu_lexicon(filename):
